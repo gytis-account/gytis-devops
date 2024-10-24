a6bd9678ab465eff58a7603f6a9956828bef2768
Gytis Preisegalavicius <gytis.preisegalavicius@nasdaq.com>
2024-10-24 17:29:58 +0300

    Adding kubernetes provider

--- atlantis.tf
+++ atlantis.tf
@@ -7,6 +7,13 @@ provider "helm" {
   }
 }
 
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+
 resource "helm_release" "atlantis" {
   name       = "atlantis"
   repository = "https://runatlantis.github.io/helm-charts"
